5b32a0cc0c2f4079d08ab63291995025f71d6d24
bhanuc <bhanuc@iitk.ac.in>
2014-05-31 07:46:39 -0700

    added first test

---
+++ test/noerror.c
@@ -0,0 +1,9 @@
+/* Hello World program */
+
+#include<stdio.h>
+
+main()
+{
+    printf("Hello World"); // THis will return the error
+
+}
---
+++ test/test.js
@@ -0,0 +1,20 @@
+var should = require('should');
+var location = __dirname;
+var nc = require('../');
+
+var callback = function(err,stdout,stderr){ 
+	if(err){
+		should.exist(err);
+	} else {
+		should.not.exist(err);
+	}
+
+};
+
+
+describe("no error compile", function(){
+it('should give no error if C file gets compiled without error', function(done) {
+	nc.compile(location+'error.c',callback);
+	done();
+});
+});
