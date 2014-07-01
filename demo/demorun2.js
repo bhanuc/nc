c23b8d19e189869c12d94826a4277ef5c1b78727
Bhanu Pratap Chaudhary <bhanu423@gmail.com>
2014-07-01 19:06:31 +0000

    updated execute2 method

---
+++ demo/demorun2.js
@@ -0,0 +1,15 @@
+var n_c = require('../');
+var location = __dirname;
+
+//A function that we want to run when the code has compiled
+var callback = function(err, data){ 
+	if(err){
+		console.log('The code encountered an error');
+		console.log(err);
+	} else {
+		console.log('The code ran successfully');
+		console.log(data);
+	}
+};
+
+var runresult = n_c.execute2(location, 'a.out', "1 \n 2 \n 3 \n", callback);
